8d857140362403498b279c8aaee538d6ccc60bbb
Georgi Manianchira <georgiman.1234@gmail.com>
2026-08-26 14:27:41 -0400

    feat: add analyze screen placeholder

---
+++ web/landing/src/AnalyzeScreen.jsx
@@ -0,0 +1,12 @@
+import './AnalyzeScreen.css'
+
+function AnalyzeScreen() {
+  return (
+    <div className="analyze-screen">
+      <h2 className="font-orbitron">Analyze</h2>
+      <p>Analyze screen coming soon</p>
+    </div>
+  )
+}
+
+export default AnalyzeScreen
